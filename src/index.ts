e436cf86568b19b5d2c8f22f9bed5e6e89ee0ba3
ikeforrest <isaah@openchair.io>
2020-10-12 15:22:08 -0500

    remove fitness tracker api

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import { NativeModules } from 'react-native';
 
-export const { RNFitnessTracker, RNHealthTracker } = NativeModules;
+export const { RNHealthTracker } = NativeModules;
 
-export {
-  HealthDataTypes,
-  UnitTypes,
-  WorkoutTypes,
-} from './types/dataTypes';
+export { HealthDataTypes, UnitTypes, WorkoutTypes } from './types/dataTypes';
 
-export { FitnessTrackerAPI as default } from './api/fitness';
 export { HealthTrackerAPI } from './api/health';
-export { PedometerAPI } from './api/pedometer';
